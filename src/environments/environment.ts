b9c5b3316eaa5901b9eaf2cf201ae68d3cbc2dd0
Diego Romero <diegitoiacc19992004@gmail.com>
2026-04-29 11:10:16 -0300

    fix: corregir import environment para produccion

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    supabaseUrl: 'https://uptyqdcogearldfbvypw.supabase.co',
+    supabaseKey: 'sb_publishable_IqcR2qz5K99bXHW2iFgLpA_6qgIZ-ec'
+};
